cf11cc28e9266df58e0d4201d52167eed0ba8fc2
BatonCHIK <anas2013tatar@gmail.com>
2026-06-06 12:56:45 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,20 @@
+const CACHE = 'batontodo-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/lang.js',
+  '/manifest.json',
+  '/sw.js',
+  '/1000050539.png',
+  '/1000050537.png'
+];
+
 self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
@@ -7,5 +23,7 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
